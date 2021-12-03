57bf5275aee0277e3b576cafb38cbf59ca9f48f6
JimenaVega <jimevega28@gmail.com>
2021-12-03 16:00:15 -0300

    some vaiables

--- src/PetriNet.java
+++ src/PetriNet.java
@@ -3,5 +3,20 @@ import java.util.Arrays;
 
 public class PetriNet{
 
-    
+    private int [][] incidenceMatrix;
+    private int [] sensitizedTrans;
+    private int [] initMarking;
+    private int [] currentMarking;
+
+    public PetriNet(int[][] inc, int[] trans,int[] initM, int[] currentM){
+
+        incidenceMatrix = inc;
+        sensitizedTrans = trans;
+        initMarking = initM;
+        currentMarking = currentM;
+
+        
+
+        
+    }
 }
